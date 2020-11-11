09f02ddbfc222a15f7f232910b18bd89f2d38610
Diego Lafuente <diego.lafuente@logtrust.com>
2020-11-11 12:22:09 +0100

    added timers

--- src/receiver-tls.js
+++ src/receiver-tls.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const path = require('path');
 
 let i = 0;
+let start = null;
 
 const server = tls.createServer({
   key: fs.readFileSync(path.join(__dirname, `./certs/collector.key`)),
@@ -22,11 +23,17 @@ server.listen(6514, '0.0.0.0', () => {
     const socketid = i++;
     let receivedData = false;
     let receivedBytes = 0;
+    if (start === null) start = Date.now();
+    console.time(`connection${socketid}`);
     console.log(`NEW CONNECTION ${socketid}`);
     socket.on('error', err => console.log(`ERROR ${socketid} - ${err}`));
     socket.on('end', () => console.log(`END ${socketid}`));
-    socket.on('close', hadError => console.log(`CLOSE ${socketid} - \
-${hadError} - RECEIVED: ${receivedBytes}`));
+    socket.on('close', hadError => {
+      console.timeEnd(`connection${socketid}`);
+      const elapsed = (Date.now() - start);
+      console.log(`CLOSE ${socketid} - ${hadError} - \
+RECEIVED: ${receivedBytes} - ELAPSED: ${elapsed}ms`);
+    });
     socket.on('data', data => {
       receivedBytes += data.length;
       if (!receivedData) {
--- src/receiver.js
+++ src/receiver.js
@@ -3,6 +3,7 @@
 const net = require('net');
 
 let i = 0;
+let start = null;
 
 const server = net.createServer();
 
@@ -11,12 +12,20 @@ server.listen(6514, '0.0.0.0', () => {
   server.on('connection', socket => {
     const socketid = i++;
     let receivedData = false;
+    let receivedBytes = 0;
+    if (start === null) start = Date.now();
+    console.time(`connection${socketid}`);
     console.log(`NEW CONNECTION ${socketid}`);
     socket.on('error', err => console.log(`ERROR ${socketid} - ${err}`));
     socket.on('end', () => console.log(`END ${socketid}`));
-    socket.on('close', hadError => console.log(`CLOSE ${socketid} - \
-${hadError}`));
-    socket.on('data', () => {
+    socket.on('close', hadError => {
+      console.timeEnd(`connection${socketid}`);
+      const elapsed = (Date.now() - start);
+      console.log(`CLOSE ${socketid} - ${hadError} - \
+RECEIVED: ${receivedBytes} - ELAPSED: ${elapsed}ms`);
+    });
+    socket.on('data', data => {
+      receivedBytes += data.length;
       if (!receivedData) {
         console.log(`RECEIVED DATA - ${socketid}`);
         receivedData = true;
